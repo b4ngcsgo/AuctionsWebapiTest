9e9f27cfd99dcc82c5a7de1cf82bc8bccae8bc45
August <august.peterson02@gmail.com>
2023-01-30 15:37:37 +0100

    Add project files.

---
+++ RemoveAuction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveAuction
+{
+    public class RemoveAuction
+    {
+        public bool TryRemoveAuction(int BidCount)
+        {
+            if (BidCount == 0)
+            {
+                return true;
+            }
+            else { return false; }
+        }
+
+    }
+}
---
+++ UnitTest2.cs
@@ -0,0 +1,22 @@
+﻿using RemoveAuction;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AuctionsTest
+{
+    public class UnitTest2
+    {
+        [Fact]
+        public void Test2()
+        {
+            //arrange
+            int BidCount = 0;
+
+            //Act
+            RemoveAuction tryRemove = new RemoveAuction();
+            bool tryRemoveResult = tryRemove.tryRemoveAuction(BidCount);
+
+            //Assert
+            Assert.True(tryRemoveResult);
+        }
+    }
+}
