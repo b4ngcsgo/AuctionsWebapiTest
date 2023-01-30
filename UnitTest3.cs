d3a06af83629fa2414e7b1d8055df545f25dd81e
August <august.peterson02@gmail.com>
2023-01-30 15:49:28 +0100

    3 av tre tester klara. En remove auction där jag testar om man kan ta bort en auktion om det inte finns några bud. O en där man testar så att man endast kan lägga bud om det är över nuvarande bud och en place bid där jag checkar användar id och den som äger auktionen id för att inte tillåta att dessa är samma.

---
+++ UnitTest3.cs
@@ -0,0 +1,23 @@
+﻿using RemoveAuction;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AuctionsTest
+{
+    public class UnitTest3
+    {
+        [Fact]
+        public void Test3()
+        {
+            //arrange
+            int bidUserID = 1;
+            int auctionOwnerUserID = 1;
+
+            //Act
+            PlaceBid bid = new PlaceBid();
+            bool tryBidResult = bid.tryPlaceBid(bidUserID, auctionOwnerUserID);
+
+            //Assert
+            Assert.True(tryRemoveResult);
+        }
+    }
+}
